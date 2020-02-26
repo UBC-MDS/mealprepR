81a9984b1caea74cf971e6e325bbf886bed4a903
SamEdwardes <edwardes.s@gmail.com>
2020-02-25 17:00:49 -0800

    Set up team work docs and make_recipe doc strings

---
+++ R/make_recipe.R
@@ -0,0 +1,26 @@
+#' make_recipe
+#'
+#' @description
+#' The `make_recipe()` function is used to quickly apply common data preprocessing techniques
+#'
+#' @param data A dataframe containing training data, validation data, X, and y
+#' @param recipe A string specifying which recipe to apply to the data
+#' @param create_train_test Boolean, if TRUE will partition data into train and test
+#' @param create_train_valid_test Boolean, if TRUE will partition data into train, valid, and test
+#'
+#' @return A list of dataframes e.g. list(train, valid, test)
+#'
+#' @section Recipes
+#'
+#' The following recipes are available currently available to pass into the `recipe` parameter:
+#'
+#' "ohe_and_standard_scaler"
+#'
+#' Apply one hot encoding to categorical features and standard scaler to numeric features
+#'
+#' @export
+#'
+#' @examples
+make_recipe <- function(data, recipe, create_train_test = FALSE, create_train_valid_test = TRUE) {
+  # insert code here
+}
