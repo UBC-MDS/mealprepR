d7afbb37dc363361b1f7abdb256e0478ab67c083
luhuayue <luhuayueapp@163.com>
2020-02-27 01:05:29 -0800

    add export and run document() and rename func name

--- R/fruits_and_veg.R
+++ R/fruits_and_veg.R
@@ -1,14 +1,17 @@
 #' fruits_and_veg Documentation
-#'
+#' 
+#' @description
 #' Example Description: Returns the names of numeric and categorical variables in the dataset
 #'
-#' @param a
-#' @param b
+#' @param df an input dataframe
+#' @param type_of_out a character indicats the wanted type (numeric/ categorical) of data
 #'
-#' @return
+#' @return a vector of index of wanted data type (numeric/ categorical)
 #' @export
 #'
 #' @examples
-fruits_and_veg <- function(a, b) {
+#' fruits_and_veg(df_demo, 'categ')
+#' >>>> c(1)
+find_fruits_veg <- function(df, type_of_out = 'categ') {
   # insert code here
 }
