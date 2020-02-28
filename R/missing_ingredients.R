5674ba83b68390f236d3591cb775390708694c3e
Jarome Leslie <31370668+jsleslie@users.noreply.github.com>
2020-02-27 16:06:33 -0800

    update to doc string format

--- R/missing_ingredients.R
+++ R/missing_ingredients.R
@@ -7,12 +7,12 @@
 #'
 #' @return A dataframe containing the columns: column_name, list_of_indices, na_count, na_proportion
 #'
-#' @export
-#'
 #' @examples
 #' missing_ingredients(df)
 #' >>>> data.frame('column_name' = c('temp','duration'), 'list_of_indices' = list(c(2,36),c(36)), 
 #'                 'na_count' = c(2, 1), 'na_proportion' = c(0.02,0.01)) 
+#'
+#' @export
 #'
 missing_ingredients <- function(data) {
   # insert code here
