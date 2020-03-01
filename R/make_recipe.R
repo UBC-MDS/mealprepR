5fe47c5d4232607d88b558ef8d4344be3e13cbf8
SamEdwardes <edwardes.s@gmail.com>
2020-03-01 11:38:54 -0800

    progress on make_recipe

--- R/make_recipe.R
+++ R/make_recipe.R
@@ -23,6 +23,55 @@
 #'
 #' @export
 
-make_recipe <- function(data, recipe, create_train_test = FALSE, create_train_valid_test = TRUE) {
-  # insert code here
+make_recipe <- function(X, y, recipe, splits_to_return="train_test") {
+
+  # validate inputs
+  testthat::test_that("An invalid parameter was entered, please review the documentation.", {
+    testthat::expect_true(nrow(X), nrow(y), label = "X and y should have the same number of observations.")
+    testthat::expect_true(recipe %in% c("ohe_and_standard_scaler"), label = "Please select a valid string option for recipe.")
+  })
+
+  # split data
+  train_valid_index <- caret::createDataPartition(y = dplyr::pull(X[,y]), p = 0.8, list = FALSE)
+  X_train_valid <- X[train_valid_index,]
+  X_test <- X[-train_valid_index,] %>% dplyr::select(-y)
+  train_index <- caret::createDataPartition(y = dplyr::pull(X_train_valid[,y]), p = 0.8, list = FALSE)
+  X_train <- X_train_valid[train_index,] %>% dplyr::select(-y)
+  X_valid <- X_train_valid[-train_index,] %>% dplyr::select(-y)
+
+  # determine column types
+  numerics <- dplyr::select_if(X_train, is.numeric) %>% colnames()
+  categorics <- dplyr::select_if(X_train, is.character) %>% colnames()
+  categorics <- c(categorics, dplyr::select_if(X_train, is.factor) %>% colnames())
+
+  # preprocess data
+  if (recipe == "ohe_and_standard_scaler") {
+    # categoricals
+    X_train <- dplyr::mutate_if(X_train, is.character, as.factor)
+    ohe <- caret::dummyVars(~., data = X_train)
+    X_train <- dplyr::as_tibble(predict(ohe, newdata = X_train)) %>%
+      janitor::clean_names()
+    # numerics
+    scaler <- X_train %>%
+      select(numerics) %>%
+      scale()
+    X_train <- X_train %>%
+      select(numerics) %>%
+      scale(center=attr(scaler, "scaled:center"),
+            scale=attr(scaler, "scaled:scale")) %>%
+      dplyr::as_tibble() %>%
+      bind_cols(select(X_train, -numerics))
+  } else {
+    stop("Please select a valid string option for recipe.")
+  }
+
+  return(list("X_train" = X_train))
 }
+
+X <- dplyr::as_tibble(mtcars) %>%
+  dplyr::mutate(carb = as.factor(carb),
+                gear = as.factor(gear),
+                vs = as.factor(vs),
+                am = as.factor(am))
+y <- "gear"
+X[,y]
