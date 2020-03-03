4df7277e355817a5fdbfc50499d1c51de94bec15
Anny <anny@annychih.com>
2020-03-02 23:07:26 -0800

    add find_bad_apples function

--- R/find_bad_apples.R
+++ R/find_bad_apples.R
@@ -2,7 +2,7 @@
 #'
 #' @description
 #' This function uses a univariate approach to outlier detection.
-#' For each column with outliers (values that are 3 or more standard deviations from the mean),
+#' For each column with outliers (values that are 2 or more standard deviations from the mean),
 #' this function will create a reference list of row indices with outliers, and
 #' the total number of outliers in that column.
 #'
@@ -10,9 +10,9 @@
 #'
 #' @param df A dataframe containing numeric data
 #'
-#' @return A dataframe with columns for 'Variable' (dataframe column name),
-#' 'Indices' (list of row indices with outliers),
-#' 'Total Outliers' (number of outliers in the column)
+#' @return A dataframe with columns for 'variable' (dataframe column name),
+#' 'total_outliers' (number of outliers in the column), and
+#' 'indices' (list of row indices with outliers)
 #'
 #' @examples
 #' find_bad_apples(df)
@@ -20,5 +20,79 @@
 #' @export
 
 find_bad_apples <- function(df) {
-  # insert code here
-}
+
+    # Returns a warning and stops the function if the dataframe includes non-numeric values
+    nums <- unlist(lapply(df, is.numeric))
+    if (dim(df)[2] != sum(nums)){
+        warning('Every column in your dataframe must be numeric.')
+        break
+    
+    # Returns a warning and stops the function if the dataframe has fewer than 30 rows
+    } else {
+        if (dim(df)[1] < 30){
+            warning('Sorry, you don\'t have enough data. The dataframe needs to have at least 30 rows.')
+            break
+            
+        } else {
+            # Creates empty output dataframe that will be added to
+            output <- tibble(variable = character(),
+                             indices = tibble(),
+                             total_outliers = integer()) %>%
+                      group_by(variable, total_outliers) %>%
+                      nest()
+            
+            columns = names(df)
+            c <- 1
+            
+            for (column in columns){
+                
+                # Finds the mean and standard deviation for each column in the dataframe
+                mean = mean(df[,c])
+                sd = sd(df[,c])
+                
+                col <- 1
+                ind <- c()
+                tot <- 0
+                
+                r <- 1
+                
+                # Evaluates each value in a column to see if it's an outlier
+                values = df[,c]
+                for(value in values){
+                    if (((mean - 2*sd) <= value & value <= (mean + 2*sd)) == TRUE){
+                        r <- r + 1
+                    } else {
+                        
+                        # Adds the indices of any found outliers to 'ind'
+                        # And adds 1 to the total number of outliers for each outlier found
+                        if (((mean - 2*sd) <= value & value <= (mean + 2*sd)) == FALSE){
+                            ind <- c(ind, r)
+                            tot <- tot + 1
+                            r <- r + 1
+                        } else {
+                            next
+                        }
+                    }
+                }
+                c <- c + 1
+                col <- col + 1
+
+                if (tot == 0){
+                    next}
+                else {
+                    # Creates a row for each column with outliers
+                    addition <- tibble(variable = column,
+                                       indices = ind,
+                                       total_outliers = tot) %>%
+                                  group_by(variable, total_outliers) %>%
+                                  nest()
+                    
+                    # Appends the row of outlier data to the 'output' dataframe
+                    output <- bind_rows(output, addition)
+                }
+                }
+        }
+    }
+    # Cleans up and returns the 'output' dataframe
+    output <- output %>% rename(indices = data)
+    return(output)
