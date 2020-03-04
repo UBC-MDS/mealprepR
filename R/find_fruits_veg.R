5c68b95aa9f881fc45b649747316a2cc454b0c7f
luhuayue <luhuayueapp@163.com>
2020-03-03 22:36:32 -0800

    add functions and test

--- R/find_fruits_veg.R
+++ R/find_fruits_veg.R
@@ -9,10 +9,27 @@
 #' @return A vector of indices of wanted data type (numeric/ categorical)
 #'
 #' @examples
-#' find_fruits_veg(df_demo, 'categ')
-#'
+#' find_fruits_veg(iris, 'num')
+#' [1] 1 2 3 4
 #' @export
-
+#'
+library(tidyr)
 find_fruits_veg <- function(df, type_of_out = 'categ') {
-  # insert code here
+  list_of_categ = c()
+  list_of_num = c()
+  df_clean = tidyr::drop_na(df)
+  if (dim(df_clean)[1] == 0) {
+    return("It is a empty data frame or too many missing data")
+  }
+  for (i in c(1:dim(df_clean)[2])){
+    if (!is.factor(df_clean[,i])){
+      list_of_num[i] = i}
+    if (is.factor(df_clean[,i])){
+      list_of_categ[i] = i}
+  }
+  if (type_of_out == 'categ'){
+    return (list_of_categ[!is.na(list_of_categ)])}
+  if (type_of_out== 'num'){
+    return (list_of_num[!is.na(list_of_num)])}
 }
+
--- tests/testthat/test-find_fruits_veg.R
+++ tests/testthat/test-find_fruits_veg.R
@@ -1,3 +1,13 @@
-test_that("multiplication works", {
-  expect_equal(2 * 2, 4)
+testthat::test_that("find_fruits_veg has the following behaviour:
+     1. If the find_fruits_veg find numerical column correctly
+     2. If the find_fruits_veg find categorical column correctly
+     3. If the find_fruits_veg can handle dataframe with NAs.
+     4. If the find_fruits_veg can handle empty dataframe.", {
+  df1 = data.frame(a =c(1:4, NA), b =c(1, NA, 3:5))
+  df2 = data.frame(a =c(1, NA), b =c(NA, 3))
+
+  testthat::expect_equal(find_fruits_veg(iris, 'num'), c(1,2,3,4))
+  testthat::expect_equal(find_fruits_veg(iris, 'categ'), c(5))
+  testthat::expect_equal(find_fruits_veg(df1, 'num'), c(1,2))
+  testthat::expect_equal(find_fruits_veg(df2, 'num'), 'It is a empty data frame or too many missing data')
 })
