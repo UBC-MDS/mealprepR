ca29ddc4de18132b7ab2d9ef3558d08903883444
Jarome Leslie <31370668+jsleslie@users.noreply.github.com>
2020-03-04 23:38:20 -0800

    swapped tibbles for data.frame's

--- tests/testthat/test-find_missing_ingredients.R
+++ tests/testthat/test-find_missing_ingredients.R
@@ -1,13 +1,13 @@
-testthat::test_that("find_fruits_veg has the following behaviour:
+testthat::test_that("find_missing_ingredients has the following behaviour:
      1. If the function find_missing_ingredients returns a string if there are no missing values
      2. If the function find_missing_ingredients returns a data frame with the correct dimensions
      3. If the function find_missing_ingredients returns a data frame with the correct column names
      4. If the function find_missing_ingredients returns a data frame with the correct NA counts
      5. If the function find_missing_ingredients returns a data frame with the correct NA proportions, {
-  tb1 = tibble("letters" = c("a","b","c"),"numbers" = c(1,2,3))
-  tb2 = tibble("letters" = c(NA,"b"),"numbers" = c(NA,NA))
+  tb1 = data.frame("letters" = c("a","b","c"),"numbers" = c(1,2,3))
+  tb2 = data.frame("letters" = c(NA,"b"),"numbers" = c(NA,NA))
   fn_result = find_missing_ingredients(tb2)
-  exp_result = tibble("Column_name"=c("letters","numbers"),"NA_count"=c(1,2),"NA_proportion"=as.factor(percent(c(.50,1))))
+  exp_result = data.frame("Column_name"=c("letters","numbers"),"NA_count"=c(1,2),"NA_proportion"=as.factor(scales::percent(c(.50,1))))
   testthat::expect_equal(find_missing_ingredients(tb1), "There are no missing values")
   testthat::expect_equal(dim(fn_result), dim(exp_result))
   testthat::expect_equal(names(fn_result), names(exp_result))
