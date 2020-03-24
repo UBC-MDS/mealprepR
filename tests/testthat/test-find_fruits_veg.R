933b07380c7ff7cbe2ce06e1bf5817cf50993fb8
luhuayue <luhuayueapp@163.com>
2020-03-24 15:04:25 -0700

    add test

--- tests/testthat/test-find_fruits_veg.R
+++ tests/testthat/test-find_fruits_veg.R
@@ -2,12 +2,16 @@ testthat::test_that("find_fruits_veg has the following behaviour:
      1. If the find_fruits_veg find numerical column correctly
      2. If the find_fruits_veg find categorical column correctly
      3. If the find_fruits_veg can handle dataframe with NAs.
-     4. If the find_fruits_veg can handle empty dataframe.", {
+     4. If the find_fruits_veg can handle empty dataframe.
+     5. If the find_fruits_veg will handle wrong input", {
   df1 = data.frame(a =c(1:4, NA), b =c(1, NA, 3:5))
   df2 = data.frame(a =c(1, NA), b =c(NA, 3))
 
   testthat::expect_equal(find_fruits_veg(iris, 'num'), c(1,2,3,4))
   testthat::expect_equal(find_fruits_veg(iris, 'categ'), c(5))
   testthat::expect_equal(find_fruits_veg(df1, 'num'), c(1,2))
-  testthat::expect_equal(find_fruits_veg(df2, 'num'), 'It is a empty data frame or too many missing data')
+  testthat::expect_equal(find_fruits_veg(df2, 'num'), 
+                         'It is a empty data frame or too many missing data')
+  testthat::expect_equal(find_fruits_veg(df1, 'xxx'), 
+                         "Wrong input of type_of_out, try 'num' or 'categ'")
 })
